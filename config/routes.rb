1fbab14694660274e0971cd8780aff81f2dd08a7
github naokisomeya <sometimes.b0105@gmail.com>
2021-05-22 22:42:00 +0900

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  root 'pages#index'
+  get 'pages/show'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
